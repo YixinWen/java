055b1e606b7618dc60a24de3bdcc5bab08dba11a
YixinWen <Yixin.Wen@foxmail.com>
2021-08-15 07:17:55 +0800

    面向对象

--- Student.java
+++
@@ -1,8 +0,0 @@
-package OOP.demo4;
-
-public class Student extends Person {
-    public void go(){
-        System.out.println("go");
-    }
-
-}
--- Teacher.java
+++
@@ -1,4 +0,0 @@
-package OOP.demo4;
-
-public class Teacher extends Person{
-}
