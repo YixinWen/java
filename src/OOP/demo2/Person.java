26e17af819cc1e9e7574a9f1c60c3fa93ae2e8f7
YixinWen <Yixin.Wen@foxmail.com>
2021-08-18 05:58:49 +0800

    update

--- src/OOP/demo2/Person.java
+++ src/OOP/demo2/Person.java
@@ -2,6 +2,15 @@ package OOP.demo2;
 
 
 public class Person {
+    public Person() {
+        System.out.println("Person无参构造执行");
+    }
+
+    protected String name = "Yixin";
+    //私有的无法被继承
+    public void print(){
+        System.out.println("Person");
+    }
 
 }
 
--- src/OOP/demo2/Student.java
+++ src/OOP/demo2/Student.java
@@ -1,4 +1,28 @@
 package OOP.demo2;
 
 public class Student extends Person {
+
+    public Student() {
+        //隐藏代码: 调用了父类的构造器
+
+        super();//调用父类构造器,必须在子类构造器的第一行
+        System.out.println("Student无参构造执行");
+    }
+
+    private String name = "xiaoming";
+
+    public void test(String name){
+        System.out.println(name);
+        System.out.println(this.name);
+        System.out.println(super.name);
+    }
+    public void print(){
+        System.out.println("Student");
+    }
+
+    public void test1(){
+        print();
+        this.print();
+//        super.print();
+    }
 }
