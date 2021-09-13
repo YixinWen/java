480bd3f92db5282d1d0f0d150b12df0c1ed9f33a
YixinWen <Yixin.Wen@foxmail.com>
2021-09-13 15:08:00 +0800

    常用方法

---
+++ src/Wrapper/WrapperMethods.java
@@ -0,0 +1,22 @@
+package Wrapper;
+/**
+ * @Author YixinWen
+ * @Date 2021/9/13 15:07
+ * 常用方法
+ **/
+public class WrapperMethods {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);//最小值
+        System.out.println(Integer.MAX_VALUE);//最大值
+
+        System.out.println(Character.isDigit('a'));//判断是否数字
+        System.out.println(Character.isLetter('a'));//判断是否字母
+        System.out.println(Character.isUpperCase('a'));//判断是否大学
+        System.out.println(Character.isLowerCase('a'));//判断是否小写
+
+        System.out.println(Character.isWhitespace('a'));//判断是否空格
+        System.out.println(Character.toUpperCase('a'));//判断是否大写
+        System.out.println(Character.toLowerCase('A'));//判断是否小写
+
+    }
+}
